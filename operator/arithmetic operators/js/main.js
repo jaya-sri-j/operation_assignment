0bdf3fc79e9b1bcd52b9ed7c216c9e317569efbf
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-01 11:26:21 +0530

    adding operation.

---
+++ operator/arithmetic operators/js/main.js
@@ -0,0 +1,25 @@
+var a=prompt("enter the value a");
+a=parseInt(a);
+
+var b=prompt("enter the value b");
+b=parseFloat(b);
+
+/*var c;
+var c=a+b;//addition
+var d=a-b;//subtraction
+var e=a*b;//multiplication
+var f=a/b;//division
+
+var h=++a;//after adding one prefix //increment
+var i=a++;
+var j=--a;//decrement
+var k=a--;
+//console.log(typeof("10"));//unary plus
+//console.log(typeof(+"10"));
+//console.log(typeof(false));
+//console.log(typeof(+false));
+//console.log(-a);//negotion*/
+var g=a**b;//exponentiation
+alert(g);
+
+ 
---
+++ operator/assignment operator/js/main.js
@@ -0,0 +1,9 @@
+var a=10;
+var b=5;
+a+=b;
+//a-=b;
+//a*=b;
+//a/=b;
+//a%=b;
+//a**=b;
+alert(a);//assignment
